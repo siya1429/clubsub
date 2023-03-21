f59499f717e0fcefc68537a0fa9bea7192e50888
siya <siya9556821429@gmail.com>
2023-03-21 18:56:57 +0530

    'authentication'

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('password_reset/', views.password_reset, name='password_reset'),
+    path('profile/', views.profile, name='profile'),
+    path('profile_edit/', views.profile_edit, name='profile_edit'),
+    path('register/', views.register, name='register'),
+
+
+]
